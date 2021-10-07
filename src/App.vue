1d6c1a085955e848ed2ca6b98ab9ea2e20685270
taiwo2 <adetonat@ymail.com>
2021-10-07 17:25:20 +0100

    Add App.vue content

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view />
+     <div class="bottom_bar">
+      <div class="row expanded">
+        <div class="sm-4 columns text-center">
+          <router-link to="/">
+            <i class="fa fa-home" aria-hidden="true"></i>
+            <span>Home</span>
+          </router-link>
+        </div>
+        <div class="sm-4 columns text-center">
+          <router-link to="/add_task">
+            <i class="fa fa-plus" aria-hidden="true" />
+            <span>Add Tasks</span>
+          </router-link>
+        </div>
+      </div>
+    </div>
   </div>
+</template>
 </template>
 
 <script>
